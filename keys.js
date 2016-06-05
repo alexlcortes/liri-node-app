95b0586770d2cd719ce43fe7682ebba30739c80f
Alejandro Cortes <Alejandro Cortes>
2016-06-05 14:32:09 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'KC2FANgRpMLZLeMWPifFPJC1O',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
