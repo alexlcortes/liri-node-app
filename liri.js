cd515402e487296795aa1d19a9493ec6ab505f8a
Alejandro Cortes <Alejandro Cortes>
2016-06-05 15:02:09 -0400

    third commit, some if statements.

---
+++ liri.js
@@ -0,0 +1,23 @@
+
+var keys = require('./keys.js');
+// var request = require('request');
+
+var ind2 = process.argv[2];
+var ind3 = process.argv[3];
+
+
+if (ind2 == "movie-this") {
+	console.log('Hold please...')
+
+} else if (ind2 == "my-tweets") {
+	console.log('Hold please...')
+
+} else if  (ind2 == "spotify-this-song") {
+	console.log('Hold please...')
+
+} else if (ind2 == "do-what-it-says") {
+	console.log('Hold please...')
+
+} else {
+	console.log("I will not do that.");
+}
