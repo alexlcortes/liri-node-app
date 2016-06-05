dec494544db66f3404b04b838c0168a12d325d5d
Alejandro Cortes <Alejandro Cortes>
2016-06-05 14:34:44 -0400

    added all keys and secrets?

--- keys.js
+++ keys.js
@@ -2,7 +2,7 @@ console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'KC2FANgRpMLZLeMWPifFPJC1O',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_secret: 'ckczshRrTfN3e7vcoEFEXcxtBDOy6nxleDPQmXqy6gKl5hHsod',
+  access_token_key: '264249865-tzUrdKruQqfuNyayOGAMECNVSqYk8prsXGsncEOR',
+  access_token_secret: 'g5qDWPasK9CXZ9tUgXnYda6befcwPI8Ap03AVFP2craCG',
 }
